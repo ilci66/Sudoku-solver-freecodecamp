52d0c40706e831e3747a6afac4ab19999944a986
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-06-07 17:36:19 +0200

    working on functional tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -12,7 +12,7 @@ suite('UnitTests', () => {
     // done();
       let errorMessage = null
       try {solver.build(validPuzzle)} 
-      catch (error) {solver = error.message}
+      catch (error) {errorMessage = error.message}
       assert.isNull(errorMessage)
       done();
 
@@ -37,12 +37,82 @@ suite('UnitTests', () => {
     done();
   })
 // Logic handles a valid row placement
+  test('valid row input', (done) => {
+    let puzzleString = puzzlesAndSolutions[0][0]
+    solver.build(puzzleString)
+    let placement = solver.checkRowPlacement(0, 3)
+    assert.isTrue(placement)
+    done();
+  })
 // Logic handles an invalid row placement
+  test('invalid row input', (done) => {
+    let puzzleString = puzzlesAndSolutions[0][0]
+    solver.build(puzzleString)
+    let placement = solver.checkRowPlacement(0, 5)
+    assert.isFalse(placement)
+    done();
+  })
 // Logic handles a valid column placement
+  test('valid column input', (done) => {
+    let puzzleString = puzzlesAndSolutions[0][0]
+    solver.build(puzzleString)
+    let placement = solver.checkColPlacement(0, 5)
+    assert.isTrue(placement)
+    done();
+  })
 // Logic handles an invalid column placement
+  test('invalid column input', (done) => {
+    let puzzleString = puzzlesAndSolutions[0][0]
+    solver.build(puzzleString)
+    let placement = solver.checkColPlacement(0, 1)
+    assert.isFalse(placement)
+    done();
+  })
 // Logic handles a valid region (3x3 grid) placement
+  test('valid region input', (done) => {
+    let puzzleString = puzzlesAndSolutions[0][0]
+    solver.build(puzzleString)
+    let placement = solver.checkColPlacement(1, 1, 1)
+    assert.isTrue(placement)
+    done();
+  })
 // Logic handles an invalid region (3x3 grid) placement
+  test('invalid region input', (done) => {
+    let puzzleString = puzzlesAndSolutions[0][0]
+    solver.build(puzzleString)
+    let placement = solver.checkColPlacement(1, 6, 2)
+    assert.isFalse(placement)
+    done();
+  })
 // Valid puzzle strings pass the solver
+  test('valid puzzle pass solver', (done) => {
+    let puzzleString = puzzlesAndSolutions[0][0]
+    let grid = solver.build(puzzleString)
+    let result = solver.solve(grid)
+    assert.equal(result, puzzlesAndSolutions[0][1])
+    done();
+  })
 // Invalid puzzle strings fail the solver
+  test('invalid puzzle pass solver', (done) => {
+    let puzzleString = puzzlesAndSolutions[0][0].replace('.',9)
+    let grid = solver.build(puzzleString)
+    // let result = solver.solve(grid)
+    let errorMessage;
+    let result;
+    try{
+      result = solver.solve(solver.build(puzzleString))
+      if (!result) throw new Error('Puzzle cannot be solved')
+    }catch(error){errorMessage = error.message}
+    assert.equal(errorMessage, 'Puzzle cannot be solved')
+    done();
+  })
 // Solver returns the the expected solution for an incomplete puzzle
+  test('valid puzzle returns solution', (done) => {
+    let puzzleString = puzzlesAndSolutions[0][0]
+    let grid = solver.build(puzzleString)
+    let result;
+    result = solver.solve(solver.build(puzzleString))
+    assert.equal(result, puzzlesAndSolutions[0][1])
+    done();
+  })
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,10 +2,37 @@ const chai = require("chai");
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server');
-
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings.js')
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
-
+// Solve a puzzle with valid puzzle string: POST request to /api/solve
+  let validPuzzleString = puzzlesAndSolutions[0][0]
+  let validPuzzleSolution = puzzlesAndSolutions[0][1]
+  test('post req, valid puzzle', (done) => {
+    let puzzle = validPuzzleString
+    chai
+      .request(server)
+      .post('/api/solve')
+      .send({puzzle})
+      .end((err, res) => {
+        assert.equal(res.status, 200)
+        assert.equal(res.body.solution, validPuzzleSolution)
+        done();
+      })
+  })
+// Solve a puzzle with missing puzzle string: POST request to /api/solve
+// Solve a puzzle with invalid characters: POST request to /api/solve
+// Solve a puzzle with incorrect length: POST request to /api/solve
+// Solve a puzzle that cannot be solved: POST request to /api/solve
+// Check a puzzle placement with all fields: POST request to /api/check
+// Check a puzzle placement with single placement conflict: POST request to /api/check
+// Check a puzzle placement with multiple placement conflicts: POST request to /api/check
+// Check a puzzle placement with all placement conflicts: POST request to /api/check
+// Check a puzzle placement with missing required fields: POST request to /api/check
+// Check a puzzle placement with invalid characters: POST request to /api/check
+// Check a puzzle placement with incorrect length: POST request to /api/check
+// Check a puzzle placement with invalid placement coordinate: POST request to /api/check
+// Check a puzzle placement with invalid placement value: POST request to /api/check
 });
 
