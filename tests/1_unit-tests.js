8aed8d05ff7eeab39fdd1a7638c39fccb423bdd5
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-06-07 15:22:59 +0200

    working on tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,21 +1,41 @@
 const chai = require('chai');
 const assert = chai.assert;
-const {puzzlesAndSolutions} = require('../controllers/puzzle-strings.js')
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings.js')
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
 suite('UnitTests', () => {
+
 // Logic handles a valid puzzle string of 81 characters
-  test('valid puzzle 81 characters', (done) => {
-    //too long
-    // let validPuzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
-    // assert.equal(solver.build(validPuzzle), '135762984946381257728459613694517832812936745357824196473298561581673429269145378')
-    let validPuzzle = puzzlesAndSolutions[0][0]
-    assert.equal(solver.build(validPuzzle), puzzlesAndSolutions[0][1])
-    done();
+  test('valid puzzle 81 characters', (done) => {   
+     let validPuzzle = puzzlesAndSolutions[0][0]
+    // assert.equal(validPuzzle, solver.build(validPuzzle))
+    // done();
+      let errorMessage = null
+      try {solver.build(validPuzzle)} 
+      catch (error) {solver = error.message}
+      assert.isNull(errorMessage)
+      done();
 
   })
+
 // Logic handles a puzzle string with invalid characters (not 1-9 or .)
+  test('invalid in puzzle string', (done) => {
+    let errorMessage;
+    let invalidPuzzle = puzzlesAndSolutions[0][0].replace(".","/")
+    try {solver.build(invalidPuzzle)}
+    catch (error) {errorMessage = error.message }
+    assert.equal(errorMessage, 'Invalid characters in puzzle')
+    done();
+  })
 // Logic handles a puzzle string that is not 81 characters in length
+  test('puzzle not 81 characters', (done) => {
+    let errorMessage;
+    let invalidPuzzle = "...11..1"
+    try{solver.build(invalidPuzzle)}
+    catch(error){errorMessage=error.message}
+    assert.equal(errorMessage, 'Expected puzzle to be 81 characters long')
+    done();
+  })
 // Logic handles a valid row placement
 // Logic handles an invalid row placement
 // Logic handles a valid column placement
